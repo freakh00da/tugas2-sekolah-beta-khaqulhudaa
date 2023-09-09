8439d13374fa1a7abca35ba18bd4e859217ed5b9
Khaqul Huda <khaqulhudaa@gmail.com>
2023-09-10 02:41:32 +0700

    update tugas ketiga

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,17 @@
     <AppHeader />
     <h1>Welcome to Tugas Sekolah Beta NuxtJs</h1>
     <p>This should be HomePage.</p>
+    <div>
+      <label for="category">Filter by Category:</label>
+      <select id="category" v-model="selectedCategory">
+        <option value="Semua">Semua</option>
+        <option value="Angka">Angka</option>
+        <option value="Huruf">Huruf</option>
+      </select>
+    </div>
+    <ul>
+      <li v-for="task in filteredTasks" :key="task.id">{{ task.title }}</li>
+    </ul>
     <FooterMenu />
   </div>
 </template>
@@ -16,6 +27,30 @@ export default {
     AppHeader,
     FooterMenu,
   },
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: '1', category: 'Angka' },
+        { id: 2, title: '2', category: 'Angka' },
+        { id: 3, title: '3', category: 'Angka' },
+        { id: 4, title: 'Satu', category: 'Huruf' },
+        { id: 5, title: 'Dua', category: 'Huruf' },
+        { id: 6, title: 'Tiga', category: 'Huruf' },
+      ],
+      selectedCategory: 'Semua',
+    }
+  },
+  computed: {
+    filteredTasks() {
+      if (this.selectedCategory === 'Semua') {
+        return this.tasks
+      } else {
+        return this.tasks.filter(
+          (task) => task.category === this.selectedCategory
+        )
+      }
+    },
+  },
 }
 </script>
 
@@ -28,5 +63,17 @@ h1 {
 p {
   font-size: 1.2rem;
   color: #666;
+}
+
+label {
+  font-weight: bold;
+  margin-right: 0.5rem;
+}
+
+select {
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 </style>
