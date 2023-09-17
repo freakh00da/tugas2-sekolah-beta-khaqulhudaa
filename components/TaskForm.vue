aabebb1f8653b7fb98bffe1a2aac7719c9cb907e
Khaqul Huda <khaqulhudaa@gmail.com>
2023-09-17 21:03:05 +0700

    update tugas 4

---
+++ components/TaskForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="task-form">
+    <form @submit.prevent="addItem">
+      <div class="form-group">
+        <label for="item">Item:</label>
+        <input id="item" v-model="newItem" type="text" />
+      </div>
+      <div class="form-group">
+        <label for="category">Category:</label>
+        <select id="category" v-model="newCategory">
+          <option value="Angka">Angka</option>
+          <option value="Huruf">Huruf</option>
+        </select>
+      </div>
+      <button type="submit">Add Item</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItem: '',
+      newCategory: '',
+    }
+  },
+  methods: {
+    addItem() {
+      if (this.newItem.trim() === '' || this.newCategory.trim() === '') {
+        alert('Kolom "Item" dan "Category" harus diisi.')
+        return
+      }
+      this.$emit('item-added', {
+        title: this.newItem,
+        category: this.newCategory,
+      })
+      this.newItem = ''
+      this.newCategory = ''
+    },
+  },
+}
+</script>
+
+<style scoped>
+.task-form {
+  text-align: left;
+}
+
+.form-group {
+  margin-bottom: 1rem;
+}
+
+label {
+  font-weight: bold;
+  margin-right: 0.5rem;
+}
+
+input,
+select {
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-right: 1rem;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,24 @@
     <AppHeader />
     <h1>Welcome to Tugas Sekolah Beta NuxtJs</h1>
     <p>This should be HomePage.</p>
+
+    <TaskForm @task-added="addTaskToList" />
+
     <div>
-      <label for="category">Filter by Category:</label>
-      <select id="category" v-model="selectedCategory">
+      <label for="filterCategory">Filter by Category:</label>
+      <select id="filterCategory" v-model="selectedCategory">
         <option value="Semua">Semua</option>
         <option value="Angka">Angka</option>
         <option value="Huruf">Huruf</option>
       </select>
     </div>
+
     <ul>
-      <li v-for="task in filteredTasks" :key="task.id">{{ task.title }}</li>
+      <li v-for="task in filteredTasks" :key="task.id">
+        {{ task.title }} - {{ task.category }}
+      </li>
     </ul>
+
     <FooterMenu />
   </div>
 </template>
@@ -21,11 +28,13 @@
 <script>
 import AppHeader from '@/components/AppHeader.vue'
 import FooterMenu from '@/components/FooterMenu.vue'
+import TaskForm from '@/components/TaskForm.vue'
 
 export default {
   components: {
     AppHeader,
     FooterMenu,
+    TaskForm,
   },
   data() {
     return {
@@ -49,6 +58,15 @@ export default {
           (task) => task.category === this.selectedCategory
         )
       }
+    },
+  },
+  methods: {
+    addTaskToList(newTask) {
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        title: newTask.title,
+        category: newTask.category,
+      })
     },
   },
 }
